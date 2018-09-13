fa302a98d85dab2cabca3b03ff13a5f0a90a5e7c
Mehret Kulala <merutaye@gmail.com>
2018-09-13 16:40:16 -0500

    done

--- Tuples/Program.cs
+++ Tuples/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Tuples
 {
@@ -6,7 +7,26 @@ namespace Tuples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<(string product, double amount, int quantity)> transactions = new List<(string, double, int)>();
+
+            transactions.Add(("skirt", 39.50, 1));
+            transactions.Add(("book", 15, 2));
+            transactions.Add(("ipod", 200, 1));
+            transactions.Add(("pants", 34.90, 6));
+            transactions.Add(("laptop", 12000, 1));
+
+            double totalRevenue = 0;
+            int totalProduct = 0;
+
+            foreach ((string products, double amount, int quantity) item in transactions)
+            {
+                totalRevenue += item.amount;
+                totalProduct += item.quantity;
+               
+            }
+            
+            Console.WriteLine($"You have sold {totalRevenue} and {totalProduct}" );
+            Console.ReadLine();
         }
     }
 }
